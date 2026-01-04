12fbbb99b81e49e91682f4a74b8bd19bc80951d8
joenash156 <joenash156@gmail.com>
2026-01-04 02:22:51 +0000

    Added some other controllers

--- backend/src/controllers/users-controllers.ts
+++ backend/src/controllers/users-controllers.ts
@@ -3,7 +3,7 @@ import { generateSlugSuffix } from "../utils/slug-suffix";
 import db from "../configs/database";
 import { Request, Response } from "express"
 import { v4 as uuid } from "uuid";
-import { RowDataPacket } from "mysql2";
+import { ResultSetHeader, RowDataPacket } from "mysql2";
 import { signAccessToken, signRefreshToken } from "../utils/jwt";
 //import "../types/express";
 
@@ -13,6 +13,15 @@ interface RequestBody {
   othername: string | null
   email: string
   password: string
+  usernameSlug: string
+  otherEmail: string
+  phone: string
+  otherPhone: string
+  bio: string
+  headline: string
+  location: string
+  role: string
+  portfolioVisibility: string
 }
 
 const REFRESH_TOKEN_EXPIRY_DAYS = 7;
@@ -80,7 +89,7 @@ export const insertUser = async (req: Request, res: Response): Promise<void> => 
 
   // insert info into database
   try{
-    await db.query<RowDataPacket[]>(insertQuery, params);
+    await db.query<ResultSetHeader>(insertQuery, params);
 
     res.status(201).json({
       success: true,
@@ -100,7 +109,7 @@ export const insertUser = async (req: Request, res: Response): Promise<void> => 
     console.error("Failed registering user!", err)
     res.status(500).json({
       success: false,
-      error: "Database error!"
+      error: "Failed registering user. Database error!"
     })
 
     return
@@ -143,7 +152,7 @@ export const getUserProfile = async (req: Request, res: Response): Promise<void>
     console.error("User not found: ", err);
     res.status(500).json({
       success: false,
-      error: "Database error"
+      error: "User not found. Database error"
     });
     return;
   }
@@ -206,6 +215,13 @@ export const loginUser = async (req: Request, res: Response): Promise<void>=> {
     // insert hashed refresh token into database
     await db.query("INSERT INTO refresh_tokens (id, user_id, token_hash, expires_at) VALUES (?, ?, ? ,?)", [id, user?.id, hashedRefreshToken, expiresAt]);
 
+    // update last login column right after the login
+    await db.query("UPDATE users SET last_login_at = NOW() WHERE id = ?", [user?.id])
+    // fetch it again to return in response
+    const [updatedCol] = await db.query<RowDataPacket[]>("SELECT last_login_at FROM users WHERE id = ?", [user?.id])
+
+    const lastLoginAt = updatedCol[0]?.last_login_at;
+
     res.status(200).json({
       success: true,
       message: "User logged in successfully!✅",
@@ -228,7 +244,7 @@ export const loginUser = async (req: Request, res: Response): Promise<void>=> {
         role: user?.role,
         portfolio_visibility: user?.portfolio_visibiltiy,
         is_approved: user?.is_approved,
-        last_login_at: user?.last_login_at,
+        last_login_at: lastLoginAt,
         is_profile_complete: user?.is_profile_complete,
         created_at: user?.created_at,
         updated_at: user?.updated_at
@@ -240,14 +256,174 @@ export const loginUser = async (req: Request, res: Response): Promise<void>=> {
     console.error("Failed to login!", err);
     res.status(500).json({
       success: false,
-      error: "Database error!"
+      error: "Failed to login user. Database error!"
     });
     return;
   }
 }
 
 // controller to update user profile (dynamically)
-// export const updateUserProfile = async (req: Request, res: Response): Promise<void> => {
-//   const userId = req.user!.id;
-//   const { firstname, lastname, username }
-// }
+export const updateUserProfile = async (req: Request, res: Response): Promise<void> => {
+  const userId = req.user!.id;
+  const { firstname, lastname, othername, usernameSlug, otherEmail, phone, otherPhone, bio, headline, location, role, portfolioVisibility}: RequestBody = req.body;
+
+  try {
+    const [rows] = await db.query<RowDataPacket[]>("SELECT id FROM users WHERE id = ?", [userId]);
+    if(rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        error: "No user found to update profile"
+      })
+      return
+    }
+
+    const fields: (string | null)[] = [];
+    const values: (string | null)[] = [];
+
+    if(firstname !== undefined) {
+      fields.push("firstname = ?");
+      values.push(firstname);
+    }
+    if(lastname !== undefined) {
+      fields.push("lastname = ?");
+      values.push(lastname);
+    }
+    if(othername !== undefined) {
+      fields.push("other_name = ?");
+      values.push(othername);
+    }
+    if(usernameSlug !== undefined) {
+      fields.push("username_slug = ?");
+      values.push(usernameSlug);
+    }
+    if(otherEmail !== undefined) {
+      fields.push("other_email = ?");
+      values.push(otherEmail);
+    }
+    if(phone !== undefined) {
+      fields.push("phone = ?");
+      values.push(phone);
+    }
+    if(otherPhone !== undefined) {
+      fields.push("other_phone = ?");
+      values.push(otherPhone);
+    }
+    if(bio !== undefined) {
+      fields.push("bio = ?");
+      values.push(bio)
+    }
+    if(headline !== undefined) {
+      fields.push("headline = ?");
+      values.push(headline)
+    }
+    if(location !== undefined) {
+      fields.push("location = ?");
+      values.push(location);
+    }
+    if(role !== undefined) {
+      fields.push("role = ?");
+      values.push(role);
+    }
+    if(portfolioVisibility !== undefined) {
+      fields.push("portfolio_visibility = ?");
+      values.push(portfolioVisibility);
+    }
+
+    if(fields.length === 0) {
+      res.status(400).json({
+        success: false,
+        error: "No field provided to update user profile"
+      })
+      return;
+    }
+
+    values.push(userId);
+
+    await db.query<ResultSetHeader>(`UPDATE users SET ${fields.join(", ")} WHERE id = ?`, values);
+
+    const [user] = await db.query<RowDataPacket[]>("SELECT id, firstname, lastname, other_name, username_slug, email, other_email, phone, other_phone, avatar_url, theme_preference, bio, headline, location, role, portfolio_visibility, is_approved, last_login_at, is_profile_complete, created_at, updated_at FROM users WHERE id = ?", [userId]);
+
+    res.status(200).json({
+      success: true,
+      message: "User profile updated successfully!✅",
+      user: user[0]
+    })
+    return;
+  } catch(err: unknown) {
+    console.error("Failed to update user profile: ", err)
+    res.status(500).json({
+      success: false,
+      error: "Failed to update user profile. Database error!"
+    })
+    return;
+  }
+
+  
+}
+
+// controller to delete user account
+export const deleteUser = async (req: Request, res: Response): Promise<void> => {
+  const userId = req.user!.id;
+  const { password }: RequestBody = req.body;
+
+  if(!password) {
+    res.status(400).json({
+      success: false,
+      error: "Password is required to delete your account!"
+    })
+    return;
+  }
+
+  try{
+    const [rows] = await db.query<RowDataPacket[]>("SELECT password_hash FROM users WHERE id = ?", [userId]);
+
+    if(rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        error: "user not found!"
+      })
+      return;
+    }
+
+    const user = rows[0];
+
+    // if user exists, we verify the user passowrd entered
+    const isMatch = await compareHashedItem(password, user?.password_hash);
+
+    if(!isMatch) {
+      res.status(401).json({
+        success: false,
+        error: "Password entered is incorrect!"
+      })
+      return;
+    }
+
+    // delete user from the datebase
+    const [results] = await db.query<ResultSetHeader>("DELETE FROM users WHERE id = ?", [userId]);
+
+    if(results.affectedRows === 0) {
+      res.status(404).json({
+        success: false,
+        error: "Unable to delete user!"
+      })
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully!✅",
+    })
+    return;
+
+  } catch(err: unknown) {
+    console.error("Failed to delete user!: ", err);
+    res.status(500).json({
+      success: false,
+      error: "Failed to delete user. Database error"
+    });
+    return;
+  }
+
+}
+
+// controller to change user password
--- backend/src/routes/users-routes.ts
+++ backend/src/routes/users-routes.ts
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import { getUserProfile, insertUser, loginUser } from "../controllers/users-controllers";
+import { deleteUser, getUserProfile, insertUser, loginUser, updateUserProfile } from "../controllers/users-controllers";
 import { requireAuth } from "../middlewares/auth.middleware";
 //import "../types/express";
 
@@ -8,10 +8,16 @@ const router: Router = express.Router();
 // router to register user
 router.post("/signup", insertUser);
 
-// router to get user profile only when logged in(protected routes)
+// router to get user profile only when logged in (protected route)
 router.get("/profile", requireAuth, getUserProfile);
 
 // router to login in user
 router.post("/login", loginUser);
+
+// router to update user profile only when logged in (protected route)
+router.patch("/update_profile", requireAuth, updateUserProfile);
+
+// router to delete user when logged in (protected route)
+router.delete("/delete", requireAuth, deleteUser);
 
 export default router;
